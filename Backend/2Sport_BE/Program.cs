dedfa78ec29bcc866ad49cd84d54f986e83c7dc3
NhanNguyen8080 <boiboii162122@gmail.com>
2024-08-29 08:55:44 +0700

    update appsettings.json

--- Backend/2Sport_BE/Program.cs
+++ Backend/2Sport_BE/Program.cs
@@ -120,11 +120,10 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 
 app.UseHttpsRedirection();
@@ -138,6 +137,6 @@ app.UseEndpoints(endpoints =>
     endpoints.MapControllers();
 });
 
-app.MapGet("/", () => "Hello");
+app.MapGet("/", () => Results.Redirect("/swagger"));
 
 app.Run();
