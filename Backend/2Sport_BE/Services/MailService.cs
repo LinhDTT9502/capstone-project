ef793e3ce25db6b01d70634c7c7ad3f719709996
Vunt369 <ngtuanvu113@gmail.com>
2024-09-26 14:57:25 +0700

    update send mail service

--- Backend/2Sport_BE/Services/MailService.cs
+++ Backend/2Sport_BE/Services/MailService.cs
@@ -108,7 +108,7 @@ namespace _2Sport_BE.Services
             bool status = false;
             try
             {
-                var templatePath = Path.Combine(Directory.GetCurrentDirectory(), "Templates", "Forgot_Password_Email.html");
+                var templatePath = Path.Combine(AppContext.BaseDirectory, "Templates", "Forgot_Password_Email.html");
                 var templateContent = await File.ReadAllTextAsync(templatePath);
 
                 var emailContent = templateContent
@@ -152,7 +152,7 @@ namespace _2Sport_BE.Services
             bool status = false;
             try
             {
-                var templatePath = Path.Combine(Directory.GetCurrentDirectory(), "Templates", "Verify_Email.html");
+                var templatePath = Path.Combine(AppContext.BaseDirectory, "Templates", "Forgot_Password_Email.html");
                 var templateContent = await File.ReadAllTextAsync(templatePath);
 
                 var emailContent = templateContent
