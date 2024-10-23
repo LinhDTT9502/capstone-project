11824742cb6783a9a8407c5eb6103f22dba84ac5
Vunt369 <ngtuanvu113@gmail.com>
2024-10-23 20:56:47 +0700

    update order model

--- Backend/2Sport_BE.Core/Models/RentalOrder.cs
+++ Backend/2Sport_BE.Core/Models/RentalOrder.cs
@@ -13,29 +13,100 @@ namespace _2Sport_BE.Repository.Models
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("RentalOrderId")]
         public int Id { get; set; }
-        public int OrderId { get; set; }
+
+        [Column("OrderCode", TypeName = "varchar")]
+        [MaxLength(50)]
+        public string? OrderCode { get; set; }
+
+        [Column("UserId")]
+        public int? UserId { get; set; }
+
+        [Column("Email")]
+        public string Email { get; set; }
+
+        [Column("ContactPhone")]
+        public string ContactPhone { get; set; }
+
+        [Column("FullName")]
+        public string FullName { get; set; }
+
+        [Column("Address")]
+        public string Address { get; set; }
+
+        [Column("BranchId")]
+        public int? BranchId { get; set; }
+
+        [Column("PaymentMethodId")]
+        public int? PaymentMethodId { get; set; }
+
+
+        [Column("SubTotal", TypeName = "decimal")]
+        public decimal? SubTotal { get; set; }
+
+
+        [Column("TranSportFee", TypeName = "decimal")]
+        public decimal? TranSportFee { get; set; }
+
+
+        [Column("TotalAmount", TypeName = "decimal")]
+        public decimal TotalAmount { get; set; }
+
+
+        [Column("Note", TypeName = "nvarchar")]
+        [MaxLength(500)]
+        public string? Note { get; set; }
+
+
+        [Column("OrderStatus")]
+        public int? OrderStatus { get; set; }
+
+
+        [Column("PaymentStatus")]
+        public string? PaymentStatus { get; set; }
+
+
+        [DisplayFormat(DataFormatString = "{0:HH-mm-ss:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.DateTime)]
+        [Column("CreatedAt")]
+        public DateTime? CreatedAt { get; set; }
+
+
+        [DisplayFormat(DataFormatString = "{0:HH-mm-ss:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.DateTime)]
+        [Column("UpdatedAt")]
+        public DateTime? UpdatedAt { get; set; }
+
 
         [DisplayFormat(DataFormatString = "{0:HH-mm-ss:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         [DataType(DataType.DateTime)]
         public DateTime? RentalStartDate { get; set; }
 
+
         [DisplayFormat(DataFormatString = "{0:HH-mm-ss:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         [DataType(DataType.DateTime)]
         public DateTime? RentalEndDate { get; set; }
+
 
         [DisplayFormat(DataFormatString = "{0:HH-mm-ss:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         [DataType(DataType.DateTime)]
         public DateTime? ReturnDate { get; set; }
 
+
         [Column("LateFee", TypeName = "decimal")]
         public decimal LateFee { get; set; }
+
         public bool IsRestocked { get; set; } // Track if the product has been restocked
         public bool IsInspected { get; set; } // Track if the product is inspected upon return
+
         [Column("DamageFee", TypeName = "decimal")]
         public decimal? DamageFee { get; set; }
-        [ForeignKey("OrderId")]
-        public virtual Order Order { get; set; }
+
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
+        public virtual PaymentMethod PaymentMethod { get; set; }
+        public virtual User User { get; set; }
+        public virtual Branch Branch { get; set; }
     }
 
 }
--- Backend/2Sport_BE.Core/Models/SaleOrder.cs
+++ Backend/2Sport_BE.Core/Models/SaleOrder.cs
@@ -5,22 +5,17 @@ using System.ComponentModel.DataAnnotations;
 
 namespace _2Sport_BE.Repository.Models;
 
-[Table("Orders")]
-public class Order
+[Table("SaleOrders")]
+public class SaleOrder
 {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("OrderId")]
     public int Id { get; set; }
 
     [Column("OrderCode", TypeName = "varchar")]
     [MaxLength(50)]
     public string? OrderCode { get; set; }
-    [Column("OrderType")]
-    public int? OrderType { get; set; } // 1: Bán, 2: Cho thuê
-
-    [Column("BranchId")]
-    public int? BranchId { get; set; }
-
     [Column("UserId")]
     public int? UserId { get; set; }
     [Column("Email")]
@@ -31,12 +26,14 @@ public class Order
     public string FullName { get; set; }
     [Column("Address")]
     public string Address { get; set; }
+    [Column("BranchId")]
+    public int? BranchId { get; set; }
 
     [Column("PaymentMethodId")]
     public int? PaymentMethodId { get; set; }
 
     [Column("SubTotal", TypeName = "decimal")]
-    public decimal SubTotal { get; set; }
+    public decimal? SubTotal { get; set; }
 
     [Column("TranSportFee", TypeName = "decimal")]
     public decimal? TranSportFee { get; set; }
@@ -48,10 +45,11 @@ public class Order
     [MaxLength(500)]
     public string? Note { get; set; }
 
-    [Column("Status")]
-    public int? Status { get; set; }
-    [Column("IsPaid")]
-    public bool? IsPaid { get; set; }
+    [Column("OrderStatus")]
+    public int? OrderStatus { get; set; }
+
+    [Column("PaymentStatus")]
+    public string? PaymentStatus { get; set; }
 
     [DisplayFormat(DataFormatString = "{0:HH-mm-ss:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
     [DataType(DataType.DateTime)]
