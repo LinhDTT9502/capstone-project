6d02b56526b0dccc7b1c5a6da37f1546d3c85ed8
Vunt369 <ngtuanvu113@gmail.com>
2024-12-21 20:45:54 +0700

    update and fix  refund UI

--- Frontend/src/api/apiUserOrder.js
+++ Frontend/src/api/apiUserOrder.js
@@ -1,9 +1,19 @@
 import axios from "axios";
 
-const API_BASE_URL = 'https://capstone-project-703387227873.asia-southeast1.run.app/api/SaleOrder';
+const API_BASE_URL = 'https://capstone-project-703387227873.asia-southeast1.run.app/api';
 
 export const getUserOrders = (id, token) => {
-  const url = `${API_BASE_URL}/get-orders-by-user?userId=${id}`;
+  const url = `${API_BASE_URL}/SaleOrder/get-orders-by-user?userId=${id}`;
+  return axios.get(url, {
+    headers: {
+      'accept': '*/*',
+      "Authorization": `Bearer ${token}`,
+    }
+  });
+};
+
+export const getUserRentalOrders = (id, token) => {
+  const url = `${API_BASE_URL}/RentalOrder/get-rental-order-by-user?userId=${id}`;
   return axios.get(url, {
     headers: {
       'accept': '*/*',
