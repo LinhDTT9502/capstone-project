b732102c06b81a3356e1d73b609a1dcfa6e85d6b
NhanNguyen8080 <boiboii162122@gmail.com>
2024-12-24 12:07:52 +0700

    migrate db

---
+++ Backend/2Sport_BE/Migrations/20241224050713_modify-fk-product-and-orderDetail-table.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _2Sport_BE.Migrations
+{
+    /// <inheritdoc />
+    public partial class modifyfkproductandorderDetailtable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderDetails_Products_ProductId",
+                table: "OrderDetails");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderDetails_ProductId",
+                table: "OrderDetails");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_ProductId",
+                table: "OrderDetails",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderDetails_Products_ProductId",
+                table: "OrderDetails",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id");
+        }
+    }
+}
